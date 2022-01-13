95a0b7b25329bdba9bcc3f003bdb502de476a682
IMRANDIL <aliimranadil2@gmail.com>
2022-01-13 20:13:15 +0530

    yh

--- server/router/router.js
+++ server/router/router.js
@@ -29,6 +29,36 @@ router.get('/showEmployee', (req, res) => {
         if (err) throw err;
         res.status(200).json({ result });
     })
+});
+
+
+
+//update the spec employee.....
+
+router.put('/updateEmployee/:id', (req, res) => {
+    const { id } = req.params;
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+    const query = `UPDATE employee SET name=?, age=?, country=?, position=?, wage=? WHERE id = ?;`;
+    connection.query(query, [name, age, country, position, wage, id], (err, result) => {
+        if (err) throw err;
+        res.status(200).send(`Successfully updated this Id:${id}`);
+    })
+});
+
+
+
+
+
+//Delete the spec employee...
+
+
+router.delete('/deleteEmployee/:id', (req, res) => {
+    const { id } = req.params;
+    const query = `DELETE FROM employee`
 })
 
 
